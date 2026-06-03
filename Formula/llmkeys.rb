e512cbd6b76e2171f20435e1a9969bdd196ae734
Mars Du <hazeflorence492@gmail.com>
2026-06-02 22:36:29 -0400

    llmkeys 0.1.1 (English-UX binary)

--- Formula/llmkeys.rb
+++ Formula/llmkeys.rb
@@ -5,12 +5,12 @@ class Llmkeys < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/csthink/llmkeys/releases/download/v0.1.0/llmkeys-0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "db9ddd6da105b7fb4ae5020ced10f09f9f547ce99db49a57aa5bb741a83a9e49"
+      url "https://github.com/csthink/llmkeys/releases/download/v0.1.1/llmkeys-0.1.1-aarch64-apple-darwin.tar.gz"
+      sha256 "b7754135431235eb3a925f6213e636c42cb30a4c12022f5df8aff46c2ab8156d"
     end
     on_intel do
-      url "https://github.com/csthink/llmkeys/releases/download/v0.1.0/llmkeys-0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "5dbc62f77a648bce62a9ec3035fa0261472ef6a964ae39ca597cbd9bf7b70724"
+      url "https://github.com/csthink/llmkeys/releases/download/v0.1.1/llmkeys-0.1.1-x86_64-apple-darwin.tar.gz"
+      sha256 "0f386ec046fde657e075cc30af2aa5e69c0d80ceeb8490702d213b8d0f87e5db"
     end
   end
 
